d4d3d1646b8c77fe5b124768a8c2c74d4a3c4161
Elizabeth A Kari <eakari@live.com>
2013-10-03 17:33:59 -0700

    initial commit + cat resource

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,60 @@
+class CatsController < ApplicationController
+  before_action :set_cat, only: [:show, :edit, :update, :destroy]
+
+  # GET /cats
+  def index
+    @cats = Cat.all
+  end
+
+  # GET /cats/1
+  def show
+  end
+
+  # GET /cats/new
+  def new
+    @cat = Cat.new
+  end
+
+  # GET /cats/1/edit
+  def edit
+  end
+
+  # POST /cats
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to @cat, notice: 'Cat was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /cats/1
+  # PATCH/PUT /cats/1.json
+  def update
+    if @cat.update(cat_params)
+      format.html redirect_to @cat, notice: 'Cat was successfully updated.'
+    else
+      format.html render action: 'edit'
+    end
+  end
+
+  # DELETE /cats/1
+  # DELETE /cats/1.json
+  def destroy
+    @cat.destroy
+    format.html redirect_to cats_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cat
+      @cat = Cat.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def cat_params
+      params.require(:cat).permit(:name, :color)
+    end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatTracker::Application.config.secret_key_base = '247879227741c52f0847eb90a0b6a02eee3096dabf99bf79b12b441ac78a2fd6ffef5182724fd096a2091f4364d61fa26a5842f314f979618c83a6093d298c9f'
